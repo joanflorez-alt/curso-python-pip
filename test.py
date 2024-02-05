30eb95265734778c021682ea0ba61970200a71b2
mac <mac@MacBook-Air-de-mac.local>
2024-02-04 20:59:03 -0500

    se agregaron test2 y test3

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 print("hola joan")
 print(13%2)
+print("joan manuel florez")
+print(2**3)
+print("233333333")
+print(12/3)
---
+++ test2.py
@@ -0,0 +1,6 @@
+print("hola manuel")
+print(13%2)
+print("joan manuel florez")
+print(2**3)
+print("233333333")
+print(12/3)
